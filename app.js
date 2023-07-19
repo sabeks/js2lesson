189a3b0628abeca34829687c0166a7e37338f0ec
saba aptsiauri <llamerchik@gmail.com>
2023-07-19 17:03:42 +0400

    jssssnomention

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log(`hello world`);
+
+const numberArrays = [2, 4, 6, 8, 10, 12];
+// console.log(numberArrays);
+numberArrays.unshift(12);
+// console.log(numberArrays);
+
+const device = [
+  { productTitle: "samsung", productPrice: 1900 },
+
+  { productTitle: "iphone", productPrice: 2000 },
+  { productTitle: "xiaomi", productPrice: 1000 },
+];
+// console.log(device);
+const arr1 = [11, 12, 13, 14, 15];
+// console.log(arr1);
+// console.log(arr1.length);
+const zoomer = [
+  {
+    phoneName: "iphone 12 pro",
+    phoneDescription: "salable product",
+    phonePrice: 2000,
+    shopAddresses: "xashuri",
+    phoneMemoryInGb: 128,
+  },
+];
+// console.log(zoomer);
+console.log(
+  "phone name is " +
+    zoomer[0].phoneName +
+    ",phone price is " +
+    zoomer[0].phonePrice +
+    ",and you can buy it at " +
+    zoomer[0].shopAddresses
+);
